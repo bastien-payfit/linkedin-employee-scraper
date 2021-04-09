70d54cdf663b57cfce13f177215395cb48b972ba
bastienvelitchkine <bastienvelitchkine@bastien.velitchkine>
2021-04-09 09:58:00 +0100

    Added docstrings to functions

--- fileInteractions.py
+++ fileInteractions.py
@@ -3,11 +3,21 @@ import pickle as pk
 import os
 
 def credsRetriever(filename:str) -> str:
+    '''
+    This function returns the email and password of a linkedin account stored in a csv file in the credentials folder.
+    @params: {str} the name of the credentials file
+    @returns: {list} a list with first the email and then the password to the linkedin account
+    '''
     with open("./credentials/" + filename, 'r') as f:
         creds = [row for row in csv.reader(f)]
     return creds[0]
 
 def openCSV(sourceFileName:str, targetFileName: str) -> list:
+    '''
+    The function opens both csv files: the one with data to scrape when the other with scraping results and returns the csv data in form of list of lists
+    @params: {str, str} the names of both files (input and result) to find in the data folder
+    @returns: {list, list} the lists of data encompassed in both files (the first being the datasource and the other being the result data)
+    '''
     with open("./data/" + sourceFileName, "r") as f2:
         dataSource = [row for row in csv.reader(f2)]
         # print(f'Data source: {dataSource}')
