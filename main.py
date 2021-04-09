27e581ddac987fb0d81efd6aa2650daedf5cb10d
bastienvelitchkine <bastienvelitchkine@bastien.velitchkine>
2021-04-09 10:29:32 +0100

    main takes locations as inputs and not xpaths anymore

--- main.py
+++ main.py
@@ -2,28 +2,29 @@ from crawling import mainCrawler
 import time
 
 
-def main(workFiles, batchSize, xpaths = ['//a[@data-control-name="visit_company_website"]/@href']):
+def main(workFiles, batchSize, locations):
     '''
     This function runs the whole scraping rutine to retrieve data from a series of linkedin urls. 
     @params: {dict, int, list}, the dict with the name of the file with linkedin account credentials and the name of the file with input data, the size of each batch of urls to process at a time and the list of xpaths  where to find data
     @returns: {None}
     '''
     startTime = time.time()
+    # We reconstitute the xpaths according to the set of locations we're interested in, starting with 'worlwide' and the total number of employees
+    nbLocations = len(locations)
+    xpaths = ['//*[@id="main"]/div[2]/div/div[1]/div[1]/span/text()']
+    for i in range(nbLocations):
+        xpaths.append(f'//span[@class="org-people-bar-graph-element__category" and contains(text(), "{locations[i]}")]/preceding-sibling::strong/text()')
+    locations = ["totalHeadcount"] + locations
     mainCrawler(workFiles, xpaths, batchSize)
     endTime = time.time()
     print(f'Runtime: {endTime - startTime}') 
 
+## PARAMETERS BELOW ##
+
 workFiles = {
-    "source": "gemeinsam1.csv",
-    "credentials": "linkedinGauvain.csv"
+    "source": "test.csv", # 👈 PUT THE NAME OF YOUR INPUT CSV FILE HERE 
+    "credentials": "linkedinBastien.csv" # 👈 PUT THE NAME OF THE CSV FILE WITH YOUR CREDENTIALS HERE (YOU COULD BE USING MULTIPLE LINKEDIN ACCOUNTS)
 }
 
-# xpath = '//a[@data-control-name="all_employees_search_link"]/text()'
-# xpath = '//span[@class="org-people-bar-graph-element__category" and contains(text(), "France")]/preceding-sibling::strong/text()'
-
-batchSize = 19
-totalEmployees = '//*[@id="main"]/div[2]/div/div[1]/div[1]/span/text()'
-germanEmployees = '//span[@class="org-people-bar-graph-element__category" and contains(text(), "Allemagne")]/preceding-sibling::strong/text()'
-
-main(workFiles, batchSize, xpaths = [germanEmployees, totalEmployees])
+main(workFiles, 1, ["Allemagne"])
 
